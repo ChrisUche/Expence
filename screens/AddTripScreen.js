8dd78695c84ba87204e6915cf69fc6c1a0f05044
Uche Chris <chrisuche58@yahoo.com>
2023-10-06 23:06:19 +0100

    inputs and clicables in the addtrip screen working

--- screens/AddTripScreen.js
+++ screens/AddTripScreen.js
@@ -1,8 +1,23 @@
 import { View, Text, SafeAreaView, Image, TextInput, TouchableOpacity } from 'react-native'
 import { colors } from '../theme'
 import BackButton from '../components/backButton'
+import { useState } from 'react'
+import { useNavigation } from '@react-navigation/core';
 
 export default function AddTripScreen() {
+    const [place, setPlace] = useState('');
+    const [country, setCountry] = useState('');
+
+    const navigation = useNavigation();
+
+    const handleAddTrip = ()=>{
+        if(place && country){
+            //good to go
+            navigation.navigate('Home')
+        }else{
+            //show error message
+        }
+    }
   return (
     <SafeAreaView>
       <View className='flex justify-between h-full mx-4'>
@@ -20,14 +35,23 @@ export default function AddTripScreen() {
                 className='w-72 h-72'/>
             </View>
             <View className='space-x-2 mx-2'>
-                <Text className ={`${colors.heading} text-lg font-bold`}>Where On Earth</Text>
-                <TextInput className='p-4 bg-white rounded-full mb-3'/>
-                <Text className ={`${colors.heading} text-lg font-bold`}>Which Country</Text>
-                <TextInput className='p-4 bg-white rounded-full mb-3'/>
+                <Text 
+                    className ={`${colors.heading} text-lg font-bold`}>Where On Earth</Text>
+                <TextInput
+                    value={place}
+                    onChangeText={value => setPlace(value)} 
+                    className='p-4 bg-white rounded-full mb-3'/>
+                <Text 
+                    className ={`${colors.heading} text-lg font-bold`}>Which Country</Text>
+                <TextInput
+                    value={country}
+                    onChangeText={value => setCountry(value)} 
+                    className='p-4 bg-white rounded-full mb-3'/>
             </View>
         </View>
         <View>
             <TouchableOpacity
+                onPress={handleAddTrip}
                 style={{backgroundColor: colors.button}}
                 className='my-6 rounded-full p-3 shadow-sm mx-2'>
                 <Text className='text-white text-lg text-center font-bold'>Add Trip</Text>
