0f21ceb5c5009bb27f6709daa2812aa9cd232f9c
Uche Chris <chrisuche58@yahoo.com>
2023-10-07 22:04:44 +0100

    trip screen ui done

--- screens/TripExpensesScreen.js
+++ screens/TripExpensesScreen.js
@@ -5,6 +5,7 @@ import randomImage from '../assets/png/randomImage'
 import EmptyList from '../components/emptyList';
 import { useNavigation } from '@react-navigation/native';
 import BackButton from '../components/backButton';
+import ExpenseCard from '../components/expenseCard';
 
 const { width } = Dimensions.get('window');
 const isSmallScreen = width <= 375;
@@ -12,46 +13,44 @@ const isSmallScreen = width <= 375;
 const items = [
     {
         id: 1,
-        place: 'Abuja',
-        country: 'Nigeria',
+        title: 'bought coffee',
+        amount: '15',
+        category: 'food',
     },
     {
         id: 2,
-        place: 'Zanzibar',
-        country: 'Tanzania',
+        title: 'bought a shirt',
+        amount: '20',
+        category: 'shopping',
     },
     {
         id: 3,
-        place: 'Paris',
-        country: 'France',
+        title: 'booked a ride',
+        amount: '80',
+        category: 'transportation',
     },
     {
         id: 4,
-        place: 'Los Angeles',
-        country: 'America',
+        title: 'watched a movie',
+        amount: '60',
+        category: 'entertaiment',
     },
-    {
-        id: 5,
-        place: 'Paris',
-        country: 'France',
-    },
-    {
-        id: 6,
-        place: 'Los Angeles',
-        country: 'America',
-    }
 ]
 
-export default function HomeScreen() {
-    const navigation = useNavigation()
+export default function TripExpensesScreen(props) {
+    const {id, place, country} = props.route.params;
+    const navigation = useNavigation();
   return (
     <SafeAreaView className="flex-1">
         <View className='px-4'>
             <View className='relative mt-5'>
-                    <View className='absolute top-0 left-0'> 
+                    <View className='absolute top-2 left-0 z-10'> 
                         <BackButton/>
                     </View>
-                    <Text className={`${colors.heading} font-bold text-xl text-center `}>Add Trip</Text>
+                    <View>
+                        <Text className={`${colors.heading} font-bold text-xl text-center `}>{place}</Text>
+                        <Text className={`${colors.heading} text-xs text-center `}>{country}</Text>
+                    </View>
             </View>
             <View className='flex-row justify-center items-center  rounded-xl mb-4'>
                 <Image 
@@ -62,7 +61,7 @@ export default function HomeScreen() {
             <View className=' space-y-3'>
                 <View className='flex-row justify-between items-center'>
                     <Text className={`${colors.heading} font-bold text-xl`}>Expenses</Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('AddTrip')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('AddExpence')}>
                         <Text className={colors.heading}>Add Expenses</Text>
                     </TouchableOpacity>
                 </View>
@@ -70,25 +69,12 @@ export default function HomeScreen() {
                     <FlatList 
                         data={items}
                         ListEmptyComponent={<EmptyList message={'You have not recorded any Expenses yet'}/>}
-                        numColumns={2}
                         keyExtractor={item => item.id}
-                        columnWrapperStyle={{
-                            justifyContent: 'space-between' //to split the trip cards
-                        }}
                         showsVerticalScrollIndicator={false}
                         className='mx-1'
                         renderItem={({item}) => {
                             return(
-                                <TouchableOpacity onPress={() => navigation.navigate('TripExpenses')} className='bg-white p-3 rounded-2xl mb-3 shadow-sm'>
-                                    <View>
-                                        <Image 
-                                            source={randomImage()}
-                                            className={`${isSmallScreen ? 'w-32 h-24 mb-2' : 'w-36 h-36 mb-2'}`} //because screen sizes differ on small screens 24 is used else 36
-                                            />
-                                        <Text className={`${colors.heading} font-bold`}>{item.place}</Text>
-                                        <Text className={`${colors.heading} text-xs`}>{item.country}</Text>
-                                    </View>
-                                </TouchableOpacity>
+                                <ExpenseCard item={item}/>
                             )
                         }}/>
                 </View>
--- theme/index.js
+++ theme/index.js
@@ -2,3 +2,11 @@ export const colors = {
     heading: "text-gray-700",
     button: '#50C878'
 }
+
+export const categoryBG = {
+    food: "#E1D3EE",
+    transportation: '#B0E3D3',
+    entertainment: '#EcFAD7',
+    shopping: '#ffdffd',
+    other: '#CAD309'
+}
